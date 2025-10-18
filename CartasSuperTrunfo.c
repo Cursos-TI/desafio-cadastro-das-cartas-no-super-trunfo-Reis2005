72bb7815d4a665592c900b473afb9bdc12139fbf
Reis2005 <reisgbm009@gmail.com>
2025-10-18 19:03:30 +0000

    Terminar Trabalho Nível Mestre Part 2!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -43,6 +43,53 @@ int vencedorAtributo(float val1, float val2, int atributo) {
 
 
 int main() {
+    // Váriaveis das cartas 
+    char estado1, estado2;
+    char codigo1[4], codigo2[4];
+    char nome_cidade1[50], nome_cidade2[50];
+    unsigned long int populacao1, populacao2;
+    float area1, area2, pib1, pib2;
+    int pontos_turisticos1, pontos_turisticos2;
+
+
+
+    // Entrada dos dados da carta 1 
+    printf("Digite o Estado da carta 1 (ex:A): ");
+    scanf("%c", &estado1);
+    printf("Digite o código da carta 1 (ex:A01): "); 
+    scanf("%s", codigo1);
+    printf("Digite o nome da cidade da carta 1: ");
+    scanf("%[^\n]", nome_cidade1);
+    printf("Digite a população da carta 1: ");
+    scanf("%lu", &populacao1);
+    printf("Digite a área da carta 1 (em km²): ");
+    scanf("%f", &area1);
+    printf("Digite o PIB da carta 1 (em bilhões de reais): ");
+    scanf("%f", &pib1);
+    printf("Digite o número de pontos turísticos da carta 1: ");
+    scanf("%d", &pontos_turisticos1);
+
+
+
+    // Entrada dos dados da carta 2 
+    printf("\nDigite o Estado da carta 2 (ex: B): ");
+    scanf(" %c, &estado2");
+    printf("Digite o código da carta 2 (ex: B02): ");
+    scanf("%s", codigo2);
+    printf("Digite  o nome da cidade da carta 2: ");
+    scanf(" %[^\n]", nome_cidade2);
+    printf("Digite a população da carta 2: ");
+    scanf("%lu", &populacao2);
+    printf("Digite a área da carta 2 (em km²): ");
+    scanf("%f", &area2);
+    printf("Digite o PIB da carta 2 (em bilhões de reais): ");
+    scanf("%f", &pib2);
+    printf("Digite o número de pontos turísticos da carta 2: ");
+    scanf("%d", &pontos_turisticos2);
+
+
+
+    
     
 }
                         
