8aef25dcc904f03dcfc166ec18b35014bdf17940
Reis2005 <reisgbm009@gmail.com>
2025-11-06 21:00:59 +0000

    Trabalho a ser finalizado.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -24,3 +24,41 @@ void inicializarTabuleiro(int tabuleiro[TAM][TAM]) {
     }
 }
 
+// Função para posicionar alguns navios (valor 3)
+void posicionarNavios(int tabuleiro[TAM][TAM]) {
+    // Apenas alguns exemplos fíxos de navios 
+    tabuleiro [2][2] = 3;
+    tabuleiro [2][3] = 3;
+    tabuleiro [2][4] = 3; 
+    tabuleiro [5][7] = 3;
+    tabuleiro [6][7] = 3;
+}
+
+// ====================== MATRIZES DE HABILIDADE ========================
+
+// Habilidade em formato de CONE (5x5) 
+void criarCone(int cone[HAB][HAB]) {
+    for (int i = 0; i < HAB; i++) {
+        for (int j = 0; j < HAB; j++) {
+            // Cria o formato de cone (topo fino, base larga)
+            if (i == 0 && j == HAB / 2) cone[i][j] = 1; 
+            else if (i == 1 && (j >= HAB / 2 - 1 && j <= HAB / 2 + 1)) cone [i][j] = 1;
+            else if (i == 2 && (j >= HAB / 2 - 2 && j <= HAB / 2 + 2)) cone [i][j] = 1;
+            else if (i > 2) cone [i][j] = 0;
+            else cone[i][j] = 0;
+        }
+    }
+}
+
+// Habilidade em formato de CRUZ (5x5) 
+void criarCruz(int cruz[HAB][HAB]) {
+    for (int i = 0; i < HAB; i++) {
+        for (int j = 0; j < HAB; j++) {
+            // Linha central ou coluna central são 1 
+            if (i == HAB / 2 || j == HAB / 2)
+               cruz[i][j] = 1;
+            else 
+               cruz[i][j] = 0;
+        }
+    }
+}
